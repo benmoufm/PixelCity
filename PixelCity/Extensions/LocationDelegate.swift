6292eacec6580ec805ae8ca5f54fe54eba6915fb
Melodie Benmouffek <m.benmouffek@gmail.com>
2018-01-22 11:21:13 +0100

    Create location delegate extension

---
+++ PixelCity/Extensions/LocationDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  LocationDelegate.swift
+//  PixelCity
+//
+//  Created by Mélodie Benmouffek on 22/01/2018.
+//  Copyright © 2018 Mélodie Benmouffek. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+extension MapViewController: CLLocationManagerDelegate {
+
+}
