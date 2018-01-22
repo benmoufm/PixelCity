01889e05a6f9395707656b50abbaa2da5d461d93
Melodie Benmouffek <m.benmouffek@gmail.com>
2018-01-22 12:03:27 +0100

    Add custom pin drop

--- PixelCity/Extensions/MapDelegate.swift
+++ PixelCity/Extensions/MapDelegate.swift
@@ -24,6 +24,18 @@ extension MapViewController: MKMapViewDelegate {
         }
     }
 
+    //MARK: - Delegate functions
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if annotation is MKUserLocation {
+            return nil
+        }
+        let pinAnnotation = MKPinAnnotationView(annotation: annotation, reuseIdentifier: "droppablePin")
+        pinAnnotation.pinTintColor = #colorLiteral(red: 0.9771530032, green: 0.7062081099, blue: 0.1748393774, alpha: 1)
+        pinAnnotation.animatesDrop = true
+        return pinAnnotation
+    }
+
+    //MARK: - @objc functions
     @objc func dropPin(sender: UITapGestureRecognizer) {
         removePin()
         let touchPoint = sender.location(in: mapView)
