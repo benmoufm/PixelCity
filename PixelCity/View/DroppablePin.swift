38a6fa089eb6e700ba30e29a9140ba2e384b06ad
Melodie Benmouffek <m.benmouffek@gmail.com>
2018-01-22 11:53:59 +0100

    Create droppable pin custom view

---
+++ PixelCity/View/DroppablePin.swift
@@ -0,0 +1,21 @@
+//
+//  DroppablePin.swift
+//  PixelCity
+//
+//  Created by Mélodie Benmouffek on 22/01/2018.
+//  Copyright © 2018 Mélodie Benmouffek. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class DroppablePin: NSObject, MKAnnotation {
+    dynamic var coordinate: CLLocationCoordinate2D
+    var identifier: String
+
+    init(coordinate: CLLocationCoordinate2D, identifier: String) {
+        self.coordinate = coordinate
+        self.identifier = identifier
+        super.init()
+    }
+}
