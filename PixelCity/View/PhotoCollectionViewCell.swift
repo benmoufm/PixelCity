d8e5799f53821d2507083a5b79f389920897a395
Melodie Benmouffek <m.benmouffek@gmail.com>
2018-01-22 17:00:39 +0100

    Add collection view cell class

---
+++ PixelCity/View/PhotoCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  PhotoCollectionViewCell.swift
+//  PixelCity
+//
+//  Created by Mélodie Benmouffek on 22/01/2018.
+//  Copyright © 2018 Mélodie Benmouffek. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
