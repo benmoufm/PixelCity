9ac65a367a77147ded0660858eaf4fdb344ed9d9
Melodie Benmouffek <m.benmouffek@gmail.com>
2018-01-22 17:23:32 +0100

    Create constants file with flickr api key

---
+++ PixelCity/Utilities/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  PixelCity
+//
+//  Created by Mélodie Benmouffek on 22/01/2018.
+//  Copyright © 2018 Mélodie Benmouffek. All rights reserved.
+//
+
+import Foundation
+
+let apiKey = "00ca9e96edae287bbca939d8722d040a"
