cdba9a965618b4608dcde76f6090bc9b52732cf9
Melodie Benmouffek <m.benmouffek@gmail.com>
2018-01-22 17:04:45 +0100

    Create extension for collection view delegate and datasource

---
+++ PixelCity/Extensions/UICollectionViewDelegate+DataSource.swift
@@ -0,0 +1,24 @@
+//
+//  UICollectionViewDelegate+DataSource.swift
+//  PixelCity
+//
+//  Created by Mélodie Benmouffek on 22/01/2018.
+//  Copyright © 2018 Mélodie Benmouffek. All rights reserved.
+//
+
+import UIKit
+
+extension MapViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "photoCell", for: indexPath) as! PhotoCollectionViewCell
+        return cell
+    }
+}
