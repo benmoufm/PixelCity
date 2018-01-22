1bde454115c5c470a1a2f3f0659c068ad12a2520
Melodie Benmouffek <m.benmouffek@gmail.com>
2018-01-22 17:31:10 +0100

    Add function to build flickr api to get photos from a certain location

--- PixelCity/Utilities/Constants.swift
+++ PixelCity/Utilities/Constants.swift
@@ -9,3 +9,7 @@
 import Foundation
 
 let apiKey = "00ca9e96edae287bbca939d8722d040a"
+
+func flickrUrl(forApiKey key: String, withAnnotation annotation: DroppablePin, addNumberOfPhotos number: Int) -> String {
+    return "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=\(key)&lat=\(annotation.coordinate.latitude)&lon=\(annotation.coordinate.longitude)&radius=1&radius_units=mi&per_page=\(number)&format=json&nojsoncallback=1"
+}
